d1db64fac070c6d02a8506fdd4689d1b2206aac1
aanupsharan <anup.ambula@gmail.com>
2019-02-17 20:08:28 -0600

    added the code for the pop() the element

--- src/com/algorithms/stack/Stack.java
+++ src/com/algorithms/stack/Stack.java
@@ -44,4 +44,15 @@ public class Stack {
         top = node;
         length++;
     }
+
+    public int pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+
+        int result = top.data;
+        top = top.next;
+        length--;
+        return result;
+    }
 }
