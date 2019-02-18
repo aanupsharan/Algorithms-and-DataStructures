f4a2ee3827bd0c0958f3bf08f6d6076a4ee4fbe3
aanupsharan <anup.ambula@gmail.com>
2019-02-17 19:27:04 -0600

    committed the code for the reversing the array

--- src/com/algorithms/array/Array.java
+++ src/com/algorithms/array/Array.java
@@ -16,9 +16,56 @@ public class Array {
         return min;
     }
 
+    //Algorithm 1: reversing the array
+    public int[] reverse(int a[], int start, int end){
+        int temp;
+        while(start < end){
+            temp = a[start];
+            a[start] = a[end];
+            a[end] = temp;
+            start++;
+            end--;
+        }
+        return a;
+    }
+
+    //Algorithm 2: reversing the array
+    public int[] reverse(int a[]){
+        if(a == null || a.length == 0){
+            System.out.println("Invalid Input");
+            return null;
+        }
+
+        int temp;
+        for(int i = 0; i < a.length; i++){
+            if(i < a.length/2){
+                temp = a[i];
+                a[i] = a[a.length-i-1];
+                a[a.length-i-1] = temp;
+            } else {
+                break;
+            }
+        }
+        return a;
+    }
+
     public static void main(String[] args){
         Array arr = new Array();
-        int[] a ={5,3,2,1,7,6};
+        int[] a ={5,3,2,1,6};
         System.out.println("Minimum element in the array is "+arr.minumum(a));
+        System.out.println("Elememts in array a:");
+        for(int i : a){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        System.out.println("Reversed elememts in array a:");
+        int[] b = arr.reverse(a,0, a.length-1);
+        if(b != null){
+            for(int i : b){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
     }
 }
